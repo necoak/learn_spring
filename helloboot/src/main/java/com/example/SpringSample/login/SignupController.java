b61df404378bb3d86896fd73a7e0ebbb98024b9b
necoak <kaneko1983g@gmail.com>
2020-05-10 10:31:36 +0900

    # SpringSample: ログイン画面遷移作成

---
+++ helloboot/src/main/java/com/example/SpringSample/login/SignupController.java
@@ -0,0 +1,24 @@
+package com.example.SpringSample.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class SignupController {
+
+    @GetMapping("/signup")
+    public String getSignUp(Model model){
+
+        // TODO: Model実装
+
+        return "login/signup";
+    }
+
+    @PostMapping("/signup")
+    public String postSignUp(Model model) {
+
+        return "redirect:/login";
+    }
+}
