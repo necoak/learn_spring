575077e1682043c0995f9e6f5e5ceb219c1c006c
necoak <kaneko1983g@gmail.com>
2021-01-03 01:20:49 +0900

    rest consuming test: done. this code can execute with Java8, can't execute Java11.

--- consuming-rest/src/main/java/com/example/consumingrest/ConsumingRestApplication.java
+++ consuming-rest/src/main/java/com/example/consumingrest/ConsumingRestApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class ConsumingRestApplication {
@@ -25,7 +29,6 @@ public class ConsumingRestApplication {
 
 	@Bean
 	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-		log.info("run start");
 		return args -> {
 			Quote quote = restTemplate.getForObject(
 					"https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
