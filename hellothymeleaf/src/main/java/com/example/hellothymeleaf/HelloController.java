02aa0f5f315cc09adad22f2edd83c572dd70f074
necoak <kaneko1983g@gmail.com>
2021-01-10 20:53:24 +0900

    thymeleaf&SpringBoot sample: GETmethod sample(fixed greeting, dynamic greeting)

--- hellothymeleaf/src/main/java/com/example/hellothymeleaf/HelloController.java
+++ hellothymeleaf/src/main/java/com/example/hellothymeleaf/HelloController.java
@@ -2,13 +2,34 @@ package com.example.hellothymeleaf;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/")
-    public String hello(Model model) {
+    public String index(Model model) {
+        return "index";
+    }
+
+    @GetMapping("/hello")
+    public String hello(Model model)  {
         return "hello";
     }
+
+    @GetMapping("/fixedgreeting")
+    public  String fixedgreeting(Model  model)  {
+        String fixedmessage = "FixedMessage";
+        model.addAttribute("message",  fixedmessage);
+        return "hello";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="message", defaultValue="NAN") String message, Model model) {
+        model.addAttribute("message", message);
+        return "hello";
+    }
+
 }
